1ba1b57fa26c0a4e51d100467f1e48e0259fcc38
Werloxx <33370985+Werloxx@users.noreply.github.com>
2017-11-04 22:32:36 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -17,5 +17,11 @@ client.on('message', message => {
   	}
 });
 
+bot.on('message', message => {
+    if (message.content === "w!prefix"){
+        bot.user.setGame('Werloxx <3')
+    }
+});
+
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
