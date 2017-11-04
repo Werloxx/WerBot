da74719c6d80259133cac3b5c754eaea4c6c2d1d
Werloxx <33370985+Werloxx@users.noreply.github.com>
2017-11-04 19:48:55 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,15 +11,5 @@ client.on('message', message => {
   	}
 });
 
-client.on('message', message => {
-    if (message.content === 'w!blague') {
-    	message.reply('test1');
-  	}
-});
-
-bot.on('ready', () => {
-    bot.user.setGame('Werloxx <3')
-  });
-
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
