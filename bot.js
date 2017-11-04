4bdf7d9f06889b9aaf7b5d96ac58e2efc3f00ae1
Werloxx <33370985+Werloxx@users.noreply.github.com>
2017-11-04 18:36:06 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,28 +5,11 @@ client.on('ready', () => {
     console.log('I am ready!');
 });
 
-var randnum = 0;
-
-bot.on('message', message => {
-    if (message.content === "petit poney"){
-        message.reply("la drogue c'est mal, ps: je suis bien placer pour dire sa :p");
-    }
+client.on('message', message => {
+    if (message.content === 'ping') {
+    	message.reply('pong');
+  	}
 });
-
-function random(min, max) {
-    min = Math.cell(0);
-    max = Math.floor(10);
-    randnum = Math.floor(Math.rando() * (max - min +1) + min);
-
-}
-
-    if (message.content === "w!blague"){
-        random();
-    
-        if (randnum == 1)(
-            message.reply("lol test")
-        )
-    }
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
