3d1037863edf2005d6098724c31ebceb2762f1ff
Werloxx <33370985+Werloxx@users.noreply.github.com>
2017-11-05 19:07:37 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,9 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+var randnum = 0;
+var prefix = ("w!");
+
 client.on('ready', () => {
     console.log('I am ready!');
 });
@@ -9,6 +12,21 @@ client.on('message', message => {
     if (message.content === 'ping') {
     	message.reply('pong');
   	}
+});
+
+bot.on('message', message => {
+    if (message.content === "w!help"){
+        message.channel.send("en creation");
+    }
+
+    if (message.content === prefix + "blague"){
+        var help_embed = new Discord.RichEmbed()
+            .setColor('#00F0D8')
+            .addField("Blague :joy:", "Pourquoi les vaches ferment les yeux pendant la traite ? Pour faire du lait concentré.")
+        message.channel.sendEmbed(help_embed)
+        //message.channel.sendMessage("tu veux voir ma bite ?")
+
+    }
 });
 
 // THIS  MUST  BE  THIS  WAY
