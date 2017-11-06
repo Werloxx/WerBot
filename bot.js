295996612deffb762d0edf4b3705261090e2b088
Werloxx <33370985+Werloxx@users.noreply.github.com>
2017-11-06 19:09:36 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,27 +3,6 @@ const client = new Discord.Client();
 
 var prefix = ("w!");
 
-var commands = {
-    "server-info": {
-      name: "server-info",
-      description: "Prints the information of the current server.",
-      extendedhelp: "I'll tell you some information about the server and the channel you're currently in.",
-      process: function(bot, msg, suffix) {
-        // if we're not in a PM, return some info about the channel
-        if (msg.channel.server) {
-          var msgArray = [];
-          msgArray.push("You are currently in " + msg.channel + " (id: " + msg.channel.id + ")");
-          msgArray.push("on server **" + msg.channel.server.name + "** (id: " + msg.channel.server.id + ") (region: " + msg.channel.server.region + ")");
-          msgArray.push("owned by " + msg.channel.server.owner + " (id: " + msg.channel.server.owner.id + ")");
-          if (msg.channel.topic) {
-            msgArray.push("The current topic is: " + msg.channel.topic);
-          }
-          bot.sendMessage(msg, msgArray);
-        } else {
-          bot.sendMessage(msg, "You can't do that in a DM, dummy!.");
-        }
-      }
-    },
 client.on('message', message => {
     if (message.content === 'Werloxx le plus beau') {
         message.reply('Ouais trop');
