5260e32909c670ab5b5f0a9fb28ee6de284393a0
Werloxx <33370985+Werloxx@users.noreply.github.com>
2017-11-11 12:51:09 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ const client = new Discord.Client();
 var prefix = ("w!");
 
 client.on('ready', () => {
-    client.user.setPresence({ game: { name: "Werloxx", type: 0} });
+    client.user.setPresence({ game: { name: "MESSAGE_ICI", type: 0} });
 console.log('I am ready!');
 });
 
@@ -29,16 +29,17 @@ client.on('message', message => {
             .setColor('#00F0D8')
             .addField("Help :joy:", "gui")
         message.channel.sendEmbed(help_embed)
-        //message.channel.sendMessage("tu veux voir ma bite ?") 
+        //message.channel.sendMessage("tu veux voir ma bite ?")
+        
     }
-    
-    bot.on('guildMemberAdd', member => {
+});
+
+bot.on('guildMemberAdd', member => {
     member.createDM().then(channel => {
       return channel.send('Bienvenue sur la PixelTeam' + member.displayName)
     }).catch(console.error)
     // On pourrait catch l'erreur autrement ici (l'utilisateur a peut être désactivé les MP)
-  })
-});
+  });
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
