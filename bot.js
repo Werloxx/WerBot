27bd9f2cf742060a2af536f05cc1a6e555ba0bfc
Werloxx <33370985+Werloxx@users.noreply.github.com>
2017-11-12 18:49:03 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -39,7 +39,7 @@ client.on('message', message => {
     if (message.content === prefix + "serveurinfos"){
         var help_embed = new Discord.RichEmbed()
             .setColor('#98B0E9')
-            .addField("Infos sur la PixelTeam :pixel__team:","		__Créateur__\rUltimate#5669")
+            .addField("Infos sur la PixelTeam :notepad_spiral:","            __Créateur__\rUltimate#5669")
         message.channel.sendEmbed(help_embed)
         //message.channel.sendMessage("tu veux voir ma bite ?")
     }
